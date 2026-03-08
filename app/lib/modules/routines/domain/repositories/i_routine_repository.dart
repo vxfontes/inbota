7303f1dd8ff7dae4244826a97c7c1dc0c6189c4e
vxfontes <nessa1vane@icloud.com>
2026-03-08 04:12:27 -0300

    - ajustes de usecases

--- app/lib/modules/routines/domain/repositories/i_routine_repository.dart
+++ app/lib/modules/routines/domain/repositories/i_routine_repository.dart
@@ -1,6 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:inbota/modules/routines/data/models/routine_exception_input.dart';
+import 'package:inbota/modules/routines/data/models/routine_exception_output.dart';
 import 'package:inbota/modules/routines/data/models/routine_list_output.dart';
+import 'package:inbota/modules/routines/data/models/routine_streak_output.dart';
+import 'package:inbota/modules/routines/data/models/routine_today_summary_output.dart';
 import 'package:inbota/shared/errors/failures.dart';
 import 'package:inbota/modules/routines/data/models/routine_completion_output.dart';
 import 'package:inbota/modules/routines/data/models/routine_create_input.dart';
